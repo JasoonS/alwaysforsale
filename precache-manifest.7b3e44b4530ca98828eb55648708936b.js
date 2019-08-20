e7222557a9e69431c2070910f0afab789475826b
Fernando Llaca <jason@smythemail.za.net>
2019-08-20 23:25:50 +0200

    Updates

---
+++ precache-manifest.7b3e44b4530ca98828eb55648708936b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c9c691f7c540173f2a1d1dac148855",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d649918cc17dbd513481",
+    "url": "./static/css/main.ffb69ed4.chunk.css"
+  },
+  {
+    "revision": "47859a0736e1a2a46e05",
+    "url": "./static/js/2.b6b2a7d4.chunk.js"
+  },
+  {
+    "revision": "d649918cc17dbd513481",
+    "url": "./static/js/main.9af5f593.chunk.js"
+  },
+  {
+    "revision": "84aae41a2e6e1322e246",
+    "url": "./static/js/runtime~main.5a01c6c9.js"
+  }
+]);
